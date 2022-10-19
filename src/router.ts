7554994fba93c530dd2627338427392d8cebb103
Lightling <contactlightling@gmail.com>
2022-10-19 18:50:37 -0400

    setup vite+vue+router

---
+++ src/router.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './components/Home.vue'
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: Home },
+  ],
+})
