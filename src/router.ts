7d4d1d903c2483d066094eeaa5678b4a593445df
Lightling <contactlightling@gmail.com>
2022-10-20 01:11:13 -0400

    remove hello world boilerplate

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import Home from './components/Home.vue'
+import Home from './views/Home/Home.vue'
 
 export default createRouter({
   history: createWebHashHistory(),
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+
+</template>
+
+<style lang='sass' scoped>
+
+</style>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+})
+</script>
