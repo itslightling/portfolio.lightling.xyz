385abaae81706123f2bccea0070392d94b072ef3
Lightling <contactlightling@gmail.com>
2022-10-20 01:09:28 -0400

    copy over inputs from 6.0.0

---
+++ src/components/inputs/Button.vue
@@ -0,0 +1,52 @@
+<template lang='pug'>
+button(
+  class='button'
+  @click='$emit("clicked")'
+)
+  div
+    span {{ text }}
+</template>
+
+<style lang='sass' scoped>
+@import 'style/variables.sass'
+
+button
+  cursor: pointer
+  background-color: $brand-light-a
+  border-radius: 0.5rem
+  position: relative
+  z-index: 5
+  padding: 0
+  border: 0.25rem solid $brand-dark-b
+  outline: 0.25rem solid transparent !important
+  transition: outline $brand-anim-a
+  &:focus
+    outline-color: $brand-dark-a !important
+  &:hover
+    outline-color: transparentize($brand-dark-a, 0.5) !important
+  &:active
+    outline-width: 0.5rem !important
+  div
+    color: $brand-light-a
+    background-color: $brand-dark-b
+    border-radius: 0.5rem
+    margin: 0.25rem
+    padding: 0.5rem
+    height: calc(100% - 1.5rem)
+    display: flex
+    align-items: center
+    justify-content: center
+</style>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
---
+++ src/components/inputs/Checkbox.vue
@@ -0,0 +1,76 @@
+<template lang='pug'>
+.checkbox
+  input(
+    :id='name'
+    :name='name'
+    type='checkbox'
+    checked
+    @change='$emit("changed", $event.target.checked)'
+  )
+  label(
+    :for='name'
+  ) {{ label }}
+</template>
+
+<style lang='sass' scoped>
+@import 'style/variables.sass'
+
+.checkbox
+  display: flex
+  width: 100%
+  align-items: center
+  *
+    cursor: pointer
+  input
+    appearance: none
+    position: relative
+    width: 3rem
+    height: 3rem
+    flex: 0 0 3rem
+    margin-right: 0.25rem
+    border: 0.25rem solid $brand-dark-b
+    outline: 0.25rem solid transparent
+    border-radius: 0.5rem
+    transition: outline $brand-anim-a
+    &:focus
+      outline-color: $brand-dark-a
+    &:hover
+      outline-color: transparentize($brand-dark-a, 0.5)
+    &:active
+      outline-width: 0.5rem
+    &::before
+      content: ''
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      background-color: transparent
+      background-size: 100% 100%
+      background-position: center center
+      background-repeat: no-repeat
+      margin: 0.25rem
+      border-radius: 0.5rem
+      transition: all $brand-anim-a
+    &:checked::before
+      background-image: url('/images/site/checkmark.svg')
+      background-size: 70% 70%
+      background-color: $brand-dark-b
+</style>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
