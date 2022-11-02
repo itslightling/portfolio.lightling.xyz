69433dfd2214531930a9a5f320e08b308256ecdc
Lightling <contactlightling@gmail.com>
2022-11-02 17:50:44 -0400

    copy over social button

---
+++ src/components/navigation/SocialButton.vue
@@ -0,0 +1,68 @@
+<template lang='pug'>
+a(
+  v-if='link',
+  :href='link',
+  :title='title',
+)
+  SocialButtonContent(
+    :title='title',
+    :image-src='imageSrc',
+    :is-wide='isWide',
+  )
+div(
+  v-else='',
+)
+  SocialButtonContent(
+    :title='title',
+    :image-src='imageSrc',
+    :is-wide='isWide',
+  )
+</template>
+
+<style lang='sass' scoped>
+@import '@/styles/variables.sass'
+
+a
+  &:hover
+    outline-color: transparentize($brand-dark-a, 0.8)
+  &:active
+    transform: scale(1.05)
+  &::after
+    display: none
+</style>
+
+<script lang='ts'>
+import {
+  defineComponent,
+} from 'vue'
+
+import SocialButtonContent from '@/components/navigation/SocialButtonContent.vue'
+
+export default defineComponent({
+  name: 'SocialButton',
+  components: {
+    SocialButtonContent,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      default: null,
+    },
+    imageSrc: {
+      type: String,
+      default: null,
+    },
+    isWide: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup () {
+
+  },
+})
+</script>
---
+++ src/components/navigation/SocialButtonContent.vue
@@ -0,0 +1,64 @@
+<template lang='pug'>
+div(
+  :class='isWide ? "social-button wide" : "social-button"',
+)
+  img(
+    :src='imageSrc',
+    alt=''
+    role='presentation',
+  )
+  p
+    | {{ title }}
+</template>
+
+<style lang='sass' scoped>
+@import '@/styles/variables.sass'
+
+.social-button
+  display: flex
+  flex-direction: column
+  align-items: center
+  grid-gap: 0
+  text-align: center
+  white-space: normal
+  &.wide
+    word-break: break-word
+    flex-direction: row
+    grid-gap: 2rem
+    text-align: left
+  img
+    height: auto
+    width: 100%
+
+.wide
+  img
+    height: 100%
+    width: auto
+</style>
+
+<script lang='ts'>
+import {
+  defineComponent,
+} from 'vue'
+
+export default defineComponent({
+  name: 'SocialButtonContent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageSrc: {
+      type: String,
+      default: null,
+    },
+    isWide: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup () {
+
+  },
+})
+</script>
