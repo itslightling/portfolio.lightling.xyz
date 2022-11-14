4fb5f148794c727112c24cd540d4510330260be1
Lightling <contactlightling@gmail.com>
2022-11-14 17:22:01 -0500

    add 404 page

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,18 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import { ProjectLink } from './types/project/ProjectLink'
 
 import About from './views/About/About.vue'
-import Home from './views/Home/Home.vue'
 import Hobbies from './views/Hobbies/Hobbies.vue'
+import Home from './views/Home/Home.vue'
+import NotFound from './views/NotFound/NotFound.vue'
 import Project from './views/Project/Project.vue'
 import Projects from './views/Projects/Projects.vue'
 
 export const SiteRoutes: ProjectLink[] & RouteRecordRaw[] = [
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound,
+    title: 'Page Not Found',
+  },
   {
     path: '/',
     component: Home,
---
+++ src/views/NotFound/NotFound.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+#main
+  .message
+    span Page not found. Return 
+    a(href='/#/') home
+    span ?
+</template>
+
+<style lang='sass'>
+#main
+  .message
+    font-size: 3rem
+    padding: 4rem 0
+    text-align: center
+</style>
